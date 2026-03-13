28b254320934e38467e7788a4e4e48cf25eb8447
Andrey A. Ugolnik <andrey@ugolnik.info>
2026-03-13 12:10:39 +0100

    chore: update version to v1.3.8

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
 {
     sConfig config;
 
-    cLog::Info("Texture Packer v1.3.7.");
+    cLog::Info("Texture Packer v1.3.8");
     cLog::Info("Copyright (c) 2017-2026 Andrey A. Ugolnik.");
     cLog::Info("");
     if (argc < 3)
